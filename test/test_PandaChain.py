f6697405ea16c6efc05873d054c58c154ca83ab4
Kenley Arai <Karai001@ucr.edu>
2018-03-25 16:18:56 -0700

    Added chain init

--- test/test_PandaChain.py
+++ test/test_PandaChain.py
@@ -1,5 +1,25 @@
 from ..PandaChain import PandaChain
+from time import time
 
 
 class TestPBlock:
-    pass
+
+    def test_init(self):
+        pc = PandaChain()
+
+        # Chain exists and has first block
+        assert len(pc.chain) == 1
+        # Chain has node transactions
+        assert len(pc.node_transactions) == 0
+
+    def test_create_block(self):
+        pass
+
+    def test_create_new_transactions(self):
+        pass
+
+    def test_create_proof_of_work(self):
+        pass
+
+    def test_end_block(self):
+        pass
